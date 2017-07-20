e16de84892e9f567408ca57217b3affb818083ca
youmjww <b4234@oic.jp>
2017-07-21 01:07:15 +0900

    vimの環境構築用スクリプトを作成

---
+++ vimSetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+// .vimrcがあったときのためにバックアップを取る
+cd ~/
+if test -e ".vimrc"; then
+	cp .vimrc .vimrc.org
+fi
+
+// vimrcをDLしてくる
+curl https://raw.githubusercontent.com/youmjww/configFile/master/.vimrc > ~/.vimrc
